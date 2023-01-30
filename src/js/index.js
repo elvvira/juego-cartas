5343cb16abb4456457310f05d8fe002cd597ac42
elvvira <elvvira.castellanos@gmail.com>
2023-01-31 00:43:41 +0100

    casa

--- src/js/index.js
+++ src/js/index.js
@@ -25,6 +25,39 @@ const compareCards = () => {
   firstCard = undefined;
   secondCard = undefined;
 };
+const createContainer = () => {
+  const fragment = document.createDocumentFragment();
+
+  for (let index = 0; index < 18; index++) {
+    const newElement = document.createElement('div');
+    newElement.classList.add('card');
+    fragment.append(newElement);
+
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('card__front');
+    newElement.append(cardFront);
+
+    const cardImage = document.createElement('img');
+
+    const randomImage = [];
+
+    for (let index = 0; index < 6; index++) {
+      const randomNumbers = Math.floor(Math.random() * 150);
+      randomImage.push(randomNumbers);
+      cardImage.src = `assets/images/${randomNumbers}.png`;
+      console.log(index);
+    }
+
+    cardFront.append(cardImage);
+
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card__back');
+    newElement.append(cardBack);
+  }
+
+  cardsContainer.append(fragment);
+};
+createContainer();
 
 cardsContainer.addEventListener('click', e => {
   e.target.classList.add('card--show');
