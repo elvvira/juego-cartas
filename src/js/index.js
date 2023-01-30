d816b699255d83aa31437dfff413f126fb2c74b5
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-01-30 15:02:15 +0100

    clase

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,47 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+const cards = document.querySelectorAll('.card');
+const cardsContainer = document.getElementById('cards-container');
+let firstCard;
+let secondCard;
 
-sayHello();
+const cardShow = () => {
+  cards.forEach(card => {
+    card.classList.add('card--show');
+  });
+  const timeoutId = setTimeout(() => {
+    cards.forEach(card => {
+      card.classList.remove('card--show');
+    });
+    clearTimeout(timeoutId);
+  }, 2000);
+};
+
+const compareCards = () => {
+  if (firstCard.dataset.number !== secondCard.dataset.number) {
+    firstCard.classList.remove('card--show');
+    secondCard.classList.remove('card--show');
+  }
+  firstCard = undefined;
+  secondCard = undefined;
+};
+
+cardsContainer.addEventListener('click', e => {
+  e.target.classList.add('card--show');
+
+  if (!firstCard) {
+    firstCard = e.target;
+  } else {
+    secondCard = e.target;
+
+    secondCard.addEventListener(
+      'transitionend',
+      () => {
+        compareCards();
+      },
+      { once: true }
+    );
+  }
+});
+
+cardShow();
